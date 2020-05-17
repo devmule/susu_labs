278e161b11866c1dfab92be32471e36aef3cfce8
DevMule <35449804+DevMule@users.noreply.github.com>
2020-05-17 19:03:56 +0500

    Revert "test"
    This reverts commit 656773d82c39e68f82a52a2da1578b20d287678f.

--- pzis_labs/lab3/Decoder.py
+++ pzis_labs/lab3/Decoder.py
@@ -1,9 +1,10 @@
-import json, math
+import json
+import math
 
 from Encoder import Encoder
-from typing import Dict
+from typing import Dict, Tuple
 import difflib  # для проверки похожести
-import random  # для метода иммитации обжига
+import random  # для метода иммитации обжига (с постоянной температурой)
 
 
 class Decoder:
@@ -17,7 +18,7 @@ class Decoder:
     json_file.close()
 
     @staticmethod
-    def decode(encoded_text: str, min_frequency: float = 0.3, max_iteration: int = 10000) -> str:
+    def decode(encoded_text: str, min_frequency: float = 0.3, max_iteration: int = 1000) -> str:
         cache: Dict[str, float] = {}
         best_frequency = 0
         best_key = ""
@@ -46,7 +47,7 @@ class Decoder:
                 f = Decoder.text_frequency(decoded)  # частота
                 # новый ключ, чтобы не повторять его в следующий раз
                 cache[new_key] = f
-                print(iteration, "[" + new_key + "]", round(f, 5), "[" + best_key + "]")
+                print(iteration, new_key, f, best_key)
 
                 if f > best_frequency:
                     # если мутант лучше чем предыдущий ключ, то мы идём в правильном направлении,
@@ -60,13 +61,13 @@ class Decoder:
     @staticmethod
     def mutate_key(key: str) -> str:
         """функция рандомизации ключа"""
-        if random.random() > .9:  # с шансом ~ 1/10 добавляется случайная буква
+        if random.random() > .9:  # с шансом 1/10 добавляется случайная буква
             r = random.randint(0, len(key))
             return key[:r] + Decoder.alphabet[random.randint(0, len(Decoder.alphabet) - 1)] + key[r:]
-        elif random.random() > .8 and len(key) > 1 or len(key) > 4:  # с шансом ~ 1/10 удаляется случайная буква
+        elif random.random() > .8 and len(key) > 1:  # с шансом 1/10 удаляется случайная буква
             r = random.randint(0, len(key) - 1)
-            return key[:r] + key[r + 1:]
-        elif random.random() > .7 and len(key) > 1:  # с шансом ~ 1/10 перемешивается
+            return key[:r] + key[r - 1:]
+        elif random.random() > .7 and len(key) > 1:  # с шансом 1/10 перемешивается
             l = list(key)
             random.shuffle(l)
             return ''.join(l)
@@ -76,39 +77,21 @@ class Decoder:
             return "".join(k)
 
     @staticmethod
-    def text_frequency(text: str, return_in_zero: int = 15) -> float:
-        """Возвращяет количество опознанных слов / на общее количество слов
-        return_in_zero - количество слов, при проверке которых,
+    def text_frequency(text: str, return_in_zero: int = 10) -> float:
+        """return_in_zero - количество слов, при проверке которых,
         если ни одно не было опознано, прекращается опознание"""
         frequency_sum = 0
-        """for i in range(Decoder.alphabet.index(" ") + 1, len(Decoder.alphabet) - 1):
-            text.replace(Decoder.alphabet[i], ' ').split()
-        while text.find('  ') != -1:
-            text.replace('  ', ' ')"""
         text = text.split(" ")
-
-        def check(check_word):
-            for d in Decoder.dictionary:  # сравнивается с каждым словом словаря
-                # если слово похоже хотя бы на 75%, то оно опознано
-                return Decoder.similarity(check_word.upper(), d.upper()) > .75
-            return False
-
         for word in text:
-            # проверяется каждое слово в тексте
             if frequency_sum >= return_in_zero and frequency_sum == 0: break
-            if check(word): frequency_sum += 1
+            for d in Decoder.dictionary:
+                if Decoder.similarity(word.upper(), d.upper()) > .75:
+                    frequency_sum += 1
         return frequency_sum / len(text)
 
     @staticmethod
     def similarity(text_1: str, text_2: str) -> float:
-        """возвращает схожесть двух слов"""
         normalized1 = text_1.lower()
         normalized2 = text_2.lower()
         matcher = difflib.SequenceMatcher(None, normalized1, normalized2)
         return matcher.ratio()
-
-
-if __name__ == "__main__":
-    encoded = Encoder.playfair_encode("hello, I am the one that can know you. What can you do?", "KEY")
-    decoded = Decoder.decode(encoded)
-    print(decoded)
