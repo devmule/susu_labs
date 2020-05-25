1013530574bd6fda9f46d3b9dc24f45821c37c19
DevMule <35449804+DevMule@users.noreply.github.com>
2020-05-25 15:11:06 +0500

    кр2

--- CHMvIR_kursach/LagrangePolynomial.c
+++ CHMvIR_kursach/LagrangePolynomial.c
@@ -16,32 +16,38 @@ double lagr(int n, double *xt, double *yt, double x) {
     return lagr_pol;
 }
 
+double f(double x) {
+    // функция для примера
+    // 2*x^4 + 3*x^3 - x^2 + 4
+    return 2 * x * x * x * x + 3 * x * x * x - x * x + 4;
+}
+
 int main(void) {
-    const int size = 8;
+    // Батятин Максим
+    // Вариант №3
+    const int size = 10; // количество координат в табличной функции
     double *xt = (double *) malloc(size * sizeof(double));
     double *yt = (double *) malloc(size * sizeof(double));
 
-    FILE *input_file = fopen("..\\input2.txt", "r");
-    FILE *output_file = fopen("..\\output2.txt", "w");
-    if (input_file == NULL || output_file == NULL) return 0;
-
+    // задание табличной функции по функции для примера
     for (int i = 0; i < size; i++) {
-        fscanf(input_file, "%lf", &xt[i]);
-        fscanf(input_file, "%lf", &yt[i]);
+        xt[i] = i;
+        yt[i] = f(i);
     }
-    fclose(input_file);
 
+    // проходим по точкам, не включенным в таблицу
+    // сравниваем полученное интерполяцией значение табличной функции
+    // со значением функции f(x) в этой же точке
+    printf("coord   :\tinterpolation,\tf(x) func\n");
     double y, x = 0.0; // от 0
     for (;;) {
         if (x > 7) break; // до 7 включительно
         else {
-            y = lagr(size, xt, yt, x);
-            printf("%f : %lf\n", x, y); // вывод в консоль
-            fprintf(output_file, "%f : %lf\n", x, y); // вывод в файл
+            y = lagr(size, xt, yt, x); // расчитываем значение у, интерполируя табличную функцию
+            // выводим в консоль значение, найденное интерполяцией табличной функции и значение f(x) в координате x
+            printf("%f:\t%lf,\t%lf\n", x, y, f(x));
             x += 0.1; // с шагом 0.1
         }
     }
-    fclose(output_file);
-
     return 0;
 }
