e28b51759c955ff82ad72a54aa08c47e0d9a23c9
Maxim Batyatin <35449804+DevMule@users.noreply.github.com>
2021-09-08 17:03:45 +0500

    ServiceOrientedArch lab1

---
+++ ServiceOrientedArch/lab1/socket-basics/TCPClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Порт нашего сервера
+            Int32 port = 13000;
+            string serverAddr = "127.0.0.1";
+            //Если адрес сервера передан как параметр
+            if (args.Length > 0) serverAddr = args[0];
+
+            try
+            {
+                TcpClient client = new TcpClient(serverAddr, port);
+
+                try
+                {
+                    Stream s = client.GetStream();
+                    StreamReader sr = new StreamReader(s);
+                    StreamWriter sw = new StreamWriter(s);
+                    sw.AutoFlush = true;
+                    Console.WriteLine(sr.ReadLine());
+                    while (true)
+                    {
+                        Console.Write("Name: ");
+                        string name = Console.ReadLine();
+                        sw.WriteLine(name);
+                        if (name == "") break;
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                    s.Close();
+                }
+
+                finally
+                {
+                    client.Close();
+                }
+
+            }
+            catch
+            {
+                Console.Write("cannot connect");
+            }
+        }
+    }
+}
---
+++ ServiceOrientedArch/lab1/socket-basics/TCPServer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static TcpListener listener;
+
+        //можем обработать сразу 5 клиентов одновременно
+        const int CLIENTS_LIMIT = 5;
+
+        //как-бы база данных
+        //словарь, где хранится вся информация о сотрудниках
+        static Dictionary<string, string> employees =
+        new Dictionary<string, string>()
+        {
+            {"john", "manager"},
+            {"jane", "steno"},
+            {"jim", "clerk"},
+            {"jack", "salesman"}
+        };
+
+        public static void Main()
+        {
+            //Порт нашего сервера
+            Int32 port = 13000;
+            //IP-адрес нашего сервера – локальная машина
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            listener = new TcpListener(localAddr, port);
+            listener.Start();
+
+            Console.WriteLine("Server mounted, listening to port {0}", port);
+
+            //We would launch our server in a separate thread
+            for (int i = 0; i < CLIENTS_LIMIT; i++)
+            {
+                Thread t = new Thread(new ThreadStart(Service));
+                t.Start();
+            }
+        }
+        public static void Service()
+        {
+            //постоянно ждем входящего соединения
+            while (true)
+            {
+                Socket soc = listener.AcceptSocket();
+
+                Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);
+                try
+                {
+                    Stream s = new NetworkStream(soc);
+                    StreamReader sr = new StreamReader(s);
+                    StreamWriter sw = new StreamWriter(s);
+                    sw.AutoFlush = true; // enable automatic flushing
+                    sw.WriteLine("{0} Employees available",
+                    employees.Count);
+
+                    while (true)
+                    {
+                        string name = sr.ReadLine();
+                        if (name == "" || name == null) break;
+                        string job = employees[name];
+                        if (job == null) job = "No such employee";
+                        sw.WriteLine(job);
+                    }
+                    s.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Disconnected: {0}", soc.RemoteEndPoint);
+                soc.Close();
+            }
+        }
+
+    }
+}
